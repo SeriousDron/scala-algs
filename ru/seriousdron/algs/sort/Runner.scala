ca622d0547263fcedebc03c6596cf43af435b651
SeriousDron <seriousdron@gmail.com>
2016-10-06 00:23:30 +0800

    Select sort algorithm

--- ru/seriousdron/algs/sort/Runner.scala
+++ ru/seriousdron/algs/sort/Runner.scala
@@ -4,13 +4,15 @@ object Runner extends App {
   val Length = 6
   val array = new Array[Int](Length)
 
+  val sortAlg = selectSort
+
   //Input
   println(s"Enter any ${array.length} numbers for Unsorted Array : ")
   for (i <- array.indices) {
     array(i) = scala.io.StdIn.readLine().toInt
   }
 
-  QuickSort.sort(array, Ordering[Int])
+  sortAlg.sort(array, Ordering[Int])
 
   //Output
   for (i <- array.indices) {
@@ -18,4 +20,23 @@ object Runner extends App {
   }
   println
 
+  def selectSort: Sort = {
+    val sortObjects = Array(InsertionSort, QuickSort)
+    while(true) {
+      //print sort types
+      println("Select sort type")
+      for (key <- sortObjects.indices) {
+        print(key + 1); print("\t"); println(sortObjects(key).getClass.getSimpleName)
+      }
+      try {
+        val input: Int = scala.io.StdIn.readInt
+        val sort: Sort = sortObjects.apply(input - 1)
+        println(s"You selected ${sort.getClass.getSimpleName}!")
+        return sort
+      } catch {
+        case e: ArrayIndexOutOfBoundsException =>
+      }
+    }
+    sortObjects(0) //stupid thing to fake return type
+  }
 }
